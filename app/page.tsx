909a2c329881c353fe8d1a0ff12fa985695f6b79
pawanp51 <pawanrp51@gmail.com>
2025-03-13 19:43:07 +0530

    added use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-{/* eslint-disable react/no-unescaped-entities */}
-
 "use client"
+{/* eslint-disable react/no-unescaped-entities */}
 
 import Image from "next/image"
 import Link from "next/link"
